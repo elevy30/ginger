beb2c428a0428ddc176d00fe11605f591c4b40cc
elevy30 <elevy30@gmail.com>
2020-05-13 14:49:16 +0300

    update spark deployment

--- helm/charts/spark/build-image.sh
+++ helm/charts/spark/build-image.sh
@@ -1,4 +1,8 @@
- #!/usr/bin/env zsh
+#!/usr/bin/env zsh
 
-./spark-2.4.5/bin/docker-image-tool.sh -r registry.local:5000 -t spark-2.4.5 build
-./spark-2.4.5/bin/docker-image-tool.sh -r registry.local:5000 -t spark-2.4.5 push
+pushd `dirname "$0"` > /dev/null
+cd spark-2.4.5/
+bin/docker-image-tool.sh -r registry.local:5000 -t 2.4.5 build
+bin/docker-image-tool.sh -r registry.local:5000 -t 2.4.5 push
+
+popd > /dev/null
